a07c6261901a39c6922a2aeb34759bb07d32b9e1
Marko Miseljic <marko.miseljic.14@gmail.com>
2021-05-19 14:13:00 +0200

    added timer

--- quizes/static/quizes/quiz.js
+++ quizes/static/quizes/quiz.js
@@ -2,6 +2,50 @@ const url = window.location.href
 const quizBox = document.getElementById('quiz-box')
 const scoreBox = document.getElementById('score-box')
 const resultBox = document.getElementById('result-box')
+const timerBox = document.getElementById('timer-box')
+
+const activateTimer = (time) => {
+    console.log(time)
+    if(time.toString().length < 2){
+        timerBox.innerHTML = `<b>0${time}:00</b>`
+    }else{
+        timerBox.innerHTML = `<b>${time}:00</b>`
+    }
+    let minutes = time - 1
+    let seconds = 60
+    let displaySeconds 
+    let displayMinutes
+
+    const timer = setInterval(()=>{
+        seconds --
+        if(seconds < 0){
+            seconds = 59
+            minutes --
+        }
+        if(minutes.toString().length < 2){
+            displayMinutes = '0'+minutes
+        }else{
+            displayMinutes = minutes
+        }
+        if(seconds.toString().length < 2){
+            displaySeconds = '0'+seconds
+        }else{
+            displaySeconds = seconds
+        }
+        if(minutes === 0 && seconds === 0){
+            timerBox.innerHTML = "<b>00:00</b>"
+            setTimeout(()=>{
+                clearInterval(timer)
+                alert('Time over')
+                sendData()
+            })
+            
+        }
+
+        timerBox.innerHTML = `<b>${displayMinutes}:${displaySeconds}</b>`
+    }, 500)
+
+}
 
 $.ajax({
     type: 'GET',
@@ -26,6 +70,7 @@ $.ajax({
                 })
             }
         });
+        activateTimer(response.time)
     },
     error: function (error) {
         console.log(error)
